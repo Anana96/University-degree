97ec581607941a52a1cb392085d23888fe34c4e3
ANNA-PC\User <anana96koz@gmail.com>
2019-02-26 21:45:19 +0300

    Игра 1. Элементы метки
    Полная синхронизация со словарем. Элементы отображены через метки(не работает)

--- EnglishVideo/Scripts/myscripts/games/DragAndDrop.js
+++ EnglishVideo/Scripts/myscripts/games/DragAndDrop.js
@@ -19,7 +19,7 @@ function getUserLogin() {
     let url = `${document.location.origin}/Account/CurrentUser`;
     let method = 'Get';
     let conn = new AjaxRequest(url, method);
-    return conn.getJson().then(response => { console.log(response); return response.UserLogin; });
+    return conn.getJson().then(response => { return response.UserLogin; });
    // console.log(` мой логин: ${login}`);
 }
 
@@ -30,137 +30,239 @@ function getWordsFromDictionary() {
         let url = `${document.location.origin}/Games/GetExpressionTranslator?userName=${login}`;
         let method = 'Get';
         let conn = new AjaxRequest(url, method);
-        conn.getJson().then(response => { console.log(response); });
+        return conn.getJson();
     });
 }
-
-getWordsFromDictionary();
 
 //-------ИГРА----------//
 
 
 
-var stage = new Konva.Stage({
-    container: 'container-konva',
-    width: document.getElementById('container-konva').offsetWidth,
-    height: document.getElementById('container-konva').offsetHeight
-});
+function startGame(words) {
+    words.then((dictionary) => {
+        console.log(dictionary);
+        createKonva(dictionary);
 
-var layer = new Konva.Layer();
+    })
+        .catch((error) => console.log(` Ошибка при отрисовки игры ${error}`));
 
-stage.add(layer);
-
-var tempLayer = new Konva.Layer();
-stage.add(tempLayer);
-
-var text = new Konva.Text({
-    fill: 'black'
-});
-layer.add(text);
-
-var star;
-for (var i = 0; i < 10; i++) {
-    star = new Konva.Star({
-        x: stage.width() * Math.random(),
-        y: stage.height() * Math.random(),
-        fill: "blue",
-        numPoints: 10,
-        innerRadius: 20,
-        outerRadius: 25,
-        draggable: true,
-        name: 'star ' + i,
-        shadowOffsetX: 5,
-        shadowOffsetY: 5
-    });
-    layer.add(star);
 }
-layer.draw();
 
-stage.on("dragstart", function (e) {
-    e.target.moveTo(tempLayer);
-    text.text('Moving ' + e.target.name());
+
+function drawRectangle(stage, word, translate) {
+
+    let color = ['#c68724', '#4cff00', '#165ca3', '#c822bc', '#808080'];
+    let valueRandomColor = Math.floor(Math.random() * color.length);
+
+    //var textOfWord = new Konva.Text({
+    //    text: word,
+    //    fontSize: 18,
+    //    fontFamily: 'Calibri',
+    //    fill: '#fff',
+    //    padding: 20,
+    //    align: 'center',
+    //});
+
+    ////определение положения фигуры
+    //let width = Math.floor(Math.random() * (stage.width() - textOfWord.getWidth()));
+    //let height = Math.floor(Math.random() * (stage.height() - textOfWord.getHeight()));
+
+    //var rect = new Konva.Rect({
+    //    stroke: '#fff',
+    //    strokeWidth: 1,
+    //    fill: color[valueRandomColor],
+    //    width: textOfWord.getWidth(),
+    //    height: textOfWord.getHeight(),
+    //    shadowColor: 'black',
+    //    shadowBlur: 10,
+    //    shadowOffset: [10, 10],
+    //    shadowOpacity: 0.1,
+    //    cornerRadius: 10,
+    //});
+
+
+
+
+
+    // simple label
+    var simpleLabel = new Konva.Label({
+        x:0,
+        y:0
+    });
+
+
+
+    simpleLabel.add(new Konva.Text({
+        text: word,
+        fontFamily: 'Calibri',
+        fontSize: 18,
+        padding: 10,
+        fill: 'black',
+        //draggable: true,
+        name: word,
+        zIndex:2
+    }));
+
+    simpleLabel.add(new Konva.Tag({
+        fill: color[valueRandomColor],
+        draggable: true
+    }));
+
+
+    let width = Math.floor(Math.random() * (stage.width() - simpleLabel.getWidth()));
+    let height = Math.floor(Math.random() * (stage.height() - simpleLabel.getHeight()));
+    simpleLabel.position({
+        x: width,
+        y: height
+    });
+
+
+    //var group = new Konva.Group({
+    //    x: width,
+    //    y: height,
+    //    draggable: true,
+    //    name: word
+    //});
+    //group.add(textOfWord, rect);
+
+    return simpleLabel;
+}
+
+function createKonva(dictionary) {
+
+    var stage = new Konva.Stage({
+        container: 'container-konva',
+        width: document.getElementById('container-konva').offsetWidth,
+        height: document.getElementById('container-konva').offsetHeight
+    });
+    var layer = new Konva.Layer();
+    stage.add(layer);
+    var tempLayer = new Konva.Layer();
+    stage.add(tempLayer);
+    //текст с указанием движения
+    var text = new Konva.Text({
+        fill: 'black'
+    });
+    layer.add(text);
+
+
+    dictionary.forEach((objectWord) => {
+        let eng = objectWord.English;
+        let rus = objectWord.Russia;
+        let rectangleEng = drawRectangle(stage,eng, rus);
+        let rectangleRus = drawRectangle(stage,rus, eng);
+        layer.add(rectangleEng);
+        layer.add(rectangleRus);
+    });
+
     layer.draw();
-});
 
 
-var previousShape;
-stage.on("dragmove", function (evt) {
-    var pos = stage.getPointerPosition();
-    var shape = layer.getIntersection(pos);
-    if (previousShape && shape) {
-        if (previousShape !== shape) {
-            // leave from old targer
-            previousShape.fire('dragleave', {
-                type: 'dragleave',
-                target: previousShape,
-                evt: evt.evt
-            }, true);
 
-            // enter new targer
+
+    //начало движения элемента
+    stage.on("dragstart", function (e) {
+        e.target.moveTo(tempLayer);
+        text.text('Moving ' + e.target.name());
+        layer.draw();
+    });
+
+    //покинутый элемент
+    var previousShape;
+
+
+    //движение
+    stage.on("dragmove", function (evt) {
+        var pos = stage.getPointerPosition();
+        var shape = layer.getIntersection(pos);
+        if (previousShape && shape) {
+            if (previousShape !== shape) {
+                // leave from old targer
+                previousShape.fire('dragleave', {
+                    type: 'dragleave',
+                    target: previousShape,
+                    evt: evt.evt
+                }, true);
+
+                // enter new targer
+                shape.fire('dragenter', {
+                    type: 'dragenter',
+                    target: shape,
+                    evt: evt.evt
+                }, true);
+                previousShape = shape;
+            } else {
+                previousShape.fire('dragover', {
+                    type: 'dragover',
+                    target: previousShape,
+                    evt: evt.evt
+                }, true);
+            }
+        } else if (!previousShape && shape) {
+            previousShape = shape;
             shape.fire('dragenter', {
                 type: 'dragenter',
                 target: shape,
                 evt: evt.evt
             }, true);
-            previousShape = shape;
-        } else {
-            previousShape.fire('dragover', {
-                type: 'dragover',
+        } else if (previousShape && !shape) {
+            previousShape.fire('dragleave', {
+                type: 'dragleave',
                 target: previousShape,
                 evt: evt.evt
             }, true);
+            previousShape = undefined;
         }
-    } else if (!previousShape && shape) {
-        previousShape = shape;
-        shape.fire('dragenter', {
-            type: 'dragenter',
-            target: shape,
-            evt: evt.evt
-        }, true);
-    } else if (previousShape && !shape) {
-        previousShape.fire('dragleave', {
-            type: 'dragleave',
-            target: previousShape,
-            evt: evt.evt
-        }, true);
+    });
+
+    //конец движения
+    stage.on("dragend", function (e) {
+        var pos = stage.getPointerPosition();
+        var shape = layer.getIntersection(pos);
+        if (shape) {
+            previousShape.fire('drop', {
+                type: 'drop',
+                target: previousShape,
+                evt: e.evt
+            }, true);
+        }
         previousShape = undefined;
-    }
-});
-stage.on("dragend", function (e) {
-    var pos = stage.getPointerPosition();
-    var shape = layer.getIntersection(pos);
-    if (shape) {
-        previousShape.fire('drop', {
-            type: 'drop',
-            target: previousShape,
-            evt: e.evt
-        }, true);
-    }
-    previousShape = undefined;
-    e.target.moveTo(layer);
-    layer.draw();
-    tempLayer.draw();
-});
+        e.target.moveTo(layer);
+        layer.draw();
+        tempLayer.draw();
+    });
 
-stage.on("dragenter", function (e) {
-    e.target.fill('green');
-    text.text('dragenter ' + e.target.name());
-    layer.draw();
-});
+    //первый раз накрывает другой
+    stage.on("dragenter", function (e) {
+        e.target.fill('green');
+        text.text('dragenter ' + e.target.name());
+        layer.draw();
+    });
 
-stage.on("dragleave", function (e) {
-    e.target.fill('blue');
-    text.text('dragleave ' + e.target.name());
-    layer.draw();
-});
+    let colorBack;
+    //покидает другой и еще держится пользователем
+    stage.on("dragleave", function (e) {
+        e.target.fill('blue');
+        text.text('dragleave ' + e.target.name());
+        layer.draw();
+    });
 
-stage.on("dragover", function (e) {
-    text.text('dragover ' + e.target.name());
-    layer.draw();
-});
+    //накрывает другой и еще держится пользователем
+    stage.on("dragover", function (e) {
+        text.text('dragover ' + e.target.name());
+        layer.draw();
+    });
 
-stage.on("drop", function (e) {
-    e.target.fill('red');
-    text.text('drop ' + e.target.name());
-    layer.draw();
-});
+    //элемент опускается на другой и остается
+    stage.on("drop", function (e) {
+        console.log(e.target);
+        e.target.fill('red');
+        text.text('drop ' + e.target.name());
+        layer.draw();
+        });
+
+}
+
+
+//точка входа
+startGame(getWordsFromDictionary());
